2d5c5702e8820144c770c401e6a54099bb9efe57
Patrick <patrsun14@gmail.com>
2025-01-24 15:05:19 -0500

    feat: add weekly distribution of returns

---
+++ DoR/weekly.py
@@ -0,0 +1,20 @@
+from components.asset import Asset
+from components.ui.data_card import DataCard
+import streamlit as st
+
+ticker = st.session_state["ticker"]
+
+@st.cache_data
+def render_page(ticker):
+    asset = Asset(ticker, interval="5d")
+    DataCard(asset).render("C-C")
+    DataCard(asset).render("H-L")
+    DataCard(asset).render("O-C")
+
+if ticker != "":
+    try:
+        render_page(ticker)
+    except: 
+        st.write("Invalid ticker symbol")
+else:
+    st.write("No data to display")
--- dev.py
+++ dev.py
@@ -5,12 +5,13 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 daily = st.Page("DoR/daily.py", title="Daily DoR")
+weekly = st.Page("DoR/weekly.py", title="Weekly DoR")
 monthly = st.Page("DoR/monthly.py", title="Monthy DoR")
 quarterly = st.Page("DoR/quarterly.py", title="Quarterly DoR")
 
 pg = st.navigation(
     {
-        "Historical Volatility": [daily, monthly, quarterly]
+        "Historical Volatility": [daily, weekly, monthly, quarterly]
     }
 )
 
