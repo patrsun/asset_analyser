1813abcfd097abcf3f77cd4e1eaf4ae9fd0f73cb
Patrick <patrsun14@gmail.com>
2025-01-24 19:56:46 -0500

    rename folder DoR -> hist_vol to represent historical volatility pages

--- init.py
+++ init.py
@@ -4,10 +4,10 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-daily = st.Page("DoR/daily.py", title="Daily DoR")
-weekly = st.Page("DoR/weekly.py", title="Weekly DoR")
-monthly = st.Page("DoR/monthly.py", title="Monthy DoR")
-quarterly = st.Page("DoR/quarterly.py", title="Quarterly DoR")
+daily = st.Page("hist_vol/daily.py", title="Daily DoR")
+weekly = st.Page("hist_vol/weekly.py", title="Weekly DoR")
+monthly = st.Page("hist_vol/monthly.py", title="Monthy DoR")
+quarterly = st.Page("hist_vol/quarterly.py", title="Quarterly DoR")
 
 pg = st.navigation(
     {
