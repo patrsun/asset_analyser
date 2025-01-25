065025002ead7b6af681e19e91eea15f694b7c6f
Patrick <patrsun14@gmail.com>
2025-01-24 19:58:57 -0500

    create average true range percentage page

--- init.py
+++ init.py
@@ -9,9 +9,11 @@ weekly = st.Page("hist_vol/weekly.py", title="Weekly DoR")
 monthly = st.Page("hist_vol/monthly.py", title="Monthy DoR")
 quarterly = st.Page("hist_vol/quarterly.py", title="Quarterly DoR")
 
+atrp = st.Page("hist_vol/atrp.py", title="Average True Range Percentage")
+
 pg = st.navigation(
     {
-        "Historical Volatility": [daily, weekly, monthly, quarterly]
+        "Historical Volatility": [daily, weekly, monthly, quarterly, atrp]
     }
 )
 
