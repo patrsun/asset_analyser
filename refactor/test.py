77910e7fd364a4d362d29d772bfd97a57714e0c9
= <patrsun14@gmail.com>
2025-02-02 23:21:11 -0500

    add ticker symbol input

--- refactor/test.py
+++ refactor/test.py
@@ -3,12 +3,13 @@ from ui import PageRenderer
 
 st.set_page_config(layout="wide")
 
+
 page = PageRenderer()
 
-def daily_page(): return page.returns_page("SPY", "1d")
-def weekly_page(): return page.returns_page("SPY", "5d")
-def monthly_page(): return page.returns_page("SPY", "1mo")
-def quarterly_page(): return page.returns_page("SPY", "3mo")
+def daily_page(): return page.returns_page(ticker, "1d")
+def weekly_page(): return page.returns_page(ticker, "5d")
+def monthly_page(): return page.returns_page(ticker, "1mo")
+def quarterly_page(): return page.returns_page(ticker, "3mo")
 
 daily = st.Page(daily_page, title="Daily DoR")
 weekly = st.Page(weekly_page, title="Weekly DoR")
@@ -20,5 +21,11 @@ pg = st.navigation(
         "Historical Volatility": [daily, weekly, monthly, quarterly]
     }
 )
+
+with st.sidebar:
+    ticker = st.text_input(
+        "Enter ticker symbol",
+        key="ticker"
+    )
 
 pg.run()
